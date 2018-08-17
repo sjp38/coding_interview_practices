834e715c6e07eb682c3f6ab8a1e14e13e57d8250
SeongJae Park <sj38.park@gmail.com>
2018-08-17 17:12:29 +0900

    Add a simple answer for practice 1.1
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ ch1/1.1/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+Check whether all character in a string is unique
+"""
+
+def no_dup_char(string):
+    """
+    Returns true if all character in string is unique,
+    false otherwise
+    """
+    sorted_str = ''.join(sorted(string))
+    old_c = None
+    for c in sorted_str:
+        if old_c == None:
+            old_c = c
+            continue
+        if c == old_c:
+            return False
+        old_c = c
+    return True
+
+if __name__ == "__main__":
+    questions = ["", "aab", "abc", "ab", "bb", "abcdef", "abcdce"]
+    answers = [True, False, True, True, False, True, False]
+
+    for i, q in enumerate(questions):
+        if no_dup_char(q) == answers[i]:
+            print "PASS [%s] test" % q
+            continue
+        print "FAIL [%s] test" % q
+        exit(1)

b98fcc872a25a305bbda7fa516d6b0059b464985
SeongJae Park <sj38.park@gmail.com>
2018-08-17 20:18:38 +0900

    Add a simple answer for practice 1.6
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ ch1/1.6/main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+"""
+Rotate a matrix by 90 degree
+"""
+
+def rotate(matrix):
+    N = len(matrix[0])
+    ret = []
+    for i in range(0, len(matrix)):
+        ret.append([0] * N)
+
+    for idx_r, r in enumerate(matrix):
+        for idx_c, field in enumerate(r):
+            row = N - 1 - idx_c
+            col = idx_r
+            ret[row][col] = field
+    return ret
+
+def pr_matrix(matrix):
+    for r in matrix:
+        print r
+
+def pr_question_answer(q, a):
+    print "questions:"
+    pr_matrix(q)
+    print "answer:"
+    pr_matrix(a)
+
+def is_same_matrix(a, b):
+    if len(a) != len(b):
+        return False
+    for i, r in enumerate(a):
+        if r != b[i]:
+            return False
+    return True
+
+if __name__ == "__main__":
+    questions = [
+                    [
+                        [1, 2, 3],
+                        [4, 5, 6],
+                        [7, 8, 9],
+                    ]
+                ]
+    answers =    [
+                   [
+                        [3, 6, 9],
+                        [2, 5, 8],
+                        [1, 4, 7],
+                    ]
+                ]
+
+    for i, q in enumerate(questions):
+        answer = rotate(q)
+        if not is_same_matrix(answer, answers[i]):
+            print "[FAIL]"
+            pr_question_answer(q, answer)
+            exit(1)
+        print "[PASS]"
+

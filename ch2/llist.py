a1d282297a890fda915ac1280c62176fba6eb919
SeongJae Park <sj38.park@gmail.com>
2018-08-23 18:40:16 +0900

    ch2: Add a python implementation of the linked list
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ ch2/llist.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+class Foo:
+
+    def __init__(self):
+        pass
+
+class Node:
+    nxt = None
+    data = None
+
+    def __init__(self, value):
+        self.nxt = None
+        self.data = value
+
+    def __str__(self):
+        values = []
+
+        n = self
+        while True:
+            values.append(n.data)
+            if n.nxt == None:
+                break
+            n = n.nxt
+        return "->".join([str(x) for x in values])
+
+    def to_pylist(self):
+        values = []
+
+        n = self
+        while True:
+            values.append(n.data)
+            if n.nxt == None:
+                break
+            n = n.nxt
+        return values
+
+    def append(self, node):
+        n = self
+        while n.nxt != None:
+            n = n.nxt
+        n.nxt = node
+
+    def delete(self, value):
+        head = self
+        n = head
+
+        if n.data == value:
+            return n.nxt
+
+        while n.nxt != None:
+            nxt = n.nxt
+            if nxt.data == value:
+                n.nxt = nxt.nxt
+                return head
+            n = n.nxt
+        return head
+
+def llst_to_lst(llst):
+    if llst == None:
+        return []
+    return llst.to_pylist()
+
+def lst_to_llst(lst):
+    if len(lst) < 1:
+        return None
+    llst = Node(lst[0])
+    for n in lst[1:]:
+        llst.append(Node(n))
+    return llst
+
+
+if __name__ == "__main__":
+    print Node(12)
